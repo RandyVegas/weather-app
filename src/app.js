757a1d0fc11dccba54872c63d4db8bfacd196089
i.tsarkov <i.tsarkov@tinkoff.ru>
2019-10-29 09:29:47 +0300

    Init commit

---
+++ src/app.js
@@ -0,0 +1,98 @@
+const path = require('path');
+const express = require('express');
+const hbs = require('hbs');
+const geocode = require('./utils/geocode');
+const forecast = require('./utils/forecast');
+
+const app = express();
+
+const publickDirectory = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
+
+app.use(express.static(publickDirectory))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'Rembo'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Rembo'
+    });
+});
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help page',
+        name: 'Rembo'
+    })
+});
+
+app.get('/products', (req, res) =>{
+    if (!req.query.search) {
+        return res.send({
+            error: 'Tou must provide a search term'
+        })
+    }
+
+    res.send({
+        products:[]
+    })
+});
+
+app.get('/weather', (req, res) => {
+    if (!req.query.address){
+        return res.send({
+            error: 'You must provide an address'
+        })
+    }
+
+    geocode(req.query.address, (error, {latitude, longitude, location} = {}) => {
+        if (error) {
+            return res.send({ error });
+        }
+
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error) {
+                return res.send({
+                    error: error
+                })
+            }
+
+            res.send({
+                location,
+                address: req.query.address,
+                forecast: forecastData
+            });
+        });
+    });
+});
+
+app.get('/help/*', (req, res) => {
+    res.render('404',{
+        title: 'Error page',
+        name: 'Rembo',
+        errorMessage: 'Help article not found.'
+    });
+});
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: 'Error page',
+        name: 'Rembo',
+        errorMessage: 'Page not found.'
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000.');
+});
